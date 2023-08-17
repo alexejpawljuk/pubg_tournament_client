41d087360cd7bad12786997934e353b6464afc8e
home <pawljuk-alexej@hotmail.com>
2023-08-17 20:38:46 +0200

    Changed Profile content backgroud

--- src/component/profile/Profile.tsx
+++ src/component/profile/Profile.tsx
@@ -3,45 +3,46 @@ import {Card, Col, Divider, Row, theme} from "antd"
 import ProfileCard from "./card/ProfileCard"
 
 const Profile = () => {
-    const {token: {colorBgContainer}} = theme.useToken()
+    const {token} = theme.useToken()
 
     return (
         // <Space style={{display: "block", width: "100%"}}>
         <>
 
-            <Row>
+            <Row style={{background: token.colorBgLayout}}>
                 <Col flex={"auto"}>
                     <Divider type="horizontal" orientation="right">
                         Profile
                     </Divider>
                 </Col>
             </Row>
-            <Row justify="center" gutter={[20, 0]} style={{background: colorBgContainer, padding: "24px", margin: 0}}>
-                <Col style={{width: 270, height: 350}} >
-                    {/*<ProfileCard />*/}
-                </Col>
-                <Col>
+            <Row justify="space-evenly" gutter={[20, 0]} style={{padding: "24px", margin: 0}}>
+                {/*<Col style={{width: 270, height: 350}} >*/}
+                {/*    /!*<ProfileCard />*!/*/}
+                {/*</Col>*/}
+                {/*<Col>*/}
                     <Card style={{width: 300, height: 300}} extra="Inventar">
 
                     </Card>
-                </Col>
-                <Col>
+                {/*</Col>*/}
+                {/*<Col>*/}
                     <Card style={{width: 300, height: 300}} extra="Friend list">
 
                     </Card>
-                </Col>
+                {/*</Col>*/}
 
-                <Col>
+                {/*<Col>*/}
                     <Card style={{width: 300, height: 300}} extra="Statistic">
 
                     </Card>
-                </Col>
+                {/*</Col>*/}
 
-                <Col>
+                {/*<Col>*/}
                     <Card style={{width: 300, height: 300}} extra="Any">
 
                     </Card>
-                </Col>
+                {/*</Col>*/}
+                <Divider ></Divider>
             </Row>
         </>
         // </Space>
