12faee677debc52b2ba428a775874ed729f8d0a3
Alexej Pawljuk <a.pawljuk@hotmail.com>
2025-05-23 11:34:05 +0200

    Hardcode image URL for shopItem on client side

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,5 +1,5 @@
 const API = {
-    URL: "https://localhost:8080"
+    URL: "http://localhost:8080"
 }
 
 
--- src/service/ShopService.tsx
+++ src/service/ShopService.tsx
@@ -2,6 +2,9 @@ import {create} from "zustand"
 import {IShopProduct} from "../component/shop/ShopProduct"
 import {shopProducts} from "../DATA/shopProduct"
 import axios from "axios"
+import {API} from "../appConfig";
+import premiumAccount from "../image/svg/high-quality.svg"
+import ticketImage from "../image/svg/ticket.svg"
 
 interface IShopService {
     products: IShopProduct[]
@@ -12,9 +15,15 @@ export const ShopService = create<IShopService>(setState => {
     return {
         products: [],
         async shopProductsFetch() {
-            const products = await shopProducts
+            // const products = await shopProducts
             // setState(state => ({...state, products }))
-            const {data} = await axios.get<IShopProduct[]>("http://localhost:8080/shop/products")
+            // const {data} = await axios.get<IShopProduct[]>("http://localhost:8080/shop/products")
+            const {data} = await axios.get<IShopProduct[]>(API.URL + "/shop/products")
+            data.map(product => {
+                if (product.title === "Premium account") product.avatarPath = premiumAccount
+                else if (product.title === "Ticket") product.avatarPath = ticketImage
+                else return product
+            })
             setState(state => ({...state, products: data}))
         }
     }
